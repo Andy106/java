59e28d19b5326ccb1371382724de9227e7f05e4e
anand <kalpananand.sharma@gmail.com>
2017-12-09 06:20:51 -0800

    My first commit

---
+++ src/main/java/com/codebind/app.java
@@ -0,0 +1,9 @@
+package com.codebind;
+
+public class app {
+	
+	public String sample() {
+		return "sample";
+	}
+	
+}
---
+++ src/test/java/com/codebind/AppTest.java
@@ -0,0 +1,7 @@
+package com.codebind;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+}
